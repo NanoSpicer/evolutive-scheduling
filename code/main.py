95793dbdd6ef1f0ebc44eed3b8e3a46f9c21c7b5
NanoSpicer <miquelrc95@outlook.es>
2021-12-04 18:14:36 +0100

    Formats code & loads data_set_lists from the fs

--- code/main.py
+++ code/main.py
@@ -12,47 +12,45 @@
 
 # Our own classes
 #
+import json
+
 import load_save as ls
 import population as pop
+
+
+
 
 
 # Main program
 #
 def process_data_sets(ds_list: list, instances_list: list):
+    for data_set_files in ds_list:
 
-	for data_set_files in ds_list:
+        ## Load data from JSON set
+        loader = ls.Loader()
+        loader.load(data_set_files)
 
-		## Load data from JSON set
-		loader = ls.Loader()
-		loader.load(data_set_files)
+        if loader.error == None:
 
-		if loader.error == None:
+            for ins in instances_list:
+                ## If JSON data set is OK, then make initial population
+                population = pop.Population(loader.data, instances=ins)
+                population.populate()
 
-			for ins in instances_list:
-				## If JSON data set is OK, then make initial population
-				population = pop.Population(loader.data, instances=ins)
-				population.populate()
+                if population.error == None:
+                    ## If population is feasible, then make the timetable
+                    population.fit()
 
-				if population.error == None:
+                    ## At the end of work, save the results
+                    saver = ls.Saver(population.results)
+                    saver.save_results(data_set_files)
 
-					## If population is feasible, then make the timetable
-					population.fit()
-
-					## At the end of work, save the results
-					saver = ls.Saver(population.results)
-					saver.save_results(data_set_files)
-
-	return
+    return
 
 
 if __name__ == '__main__':
+    data_set_list = list(map(lambda index: f"../inputs/test{index}", range(1, 5)))
 
-	data_set_list = [
-		'../data/set_1',
-#        '../data/set_2'
-		]
+    instances_list = [10, 50]
 
-	instances_list = [10, 50]
-
-	process_data_sets(data_set_list, instances_list)
-
+    process_data_sets(data_set_list, instances_list)
