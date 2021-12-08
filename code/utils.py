de5c47131acceb977ed7b5c40fa1c0eaa3a592c6
NanoSpicer <miquelrc95@outlook.es>
2021-12-08 20:30:52 +0100

    Adds utility functions to create pairs

---
+++ code/utils.py
@@ -0,0 +1,22 @@
+
+
+def pairwise(items: list) -> list:
+    previous = None
+    pairs = []
+    for current in items:
+        pairs.append((previous, current))
+        previous = current
+    return pairs[1:]  # remove first element as previous is going to be none
+
+def items_as_pairs(items: list) -> list:
+    por_pares = pairwise(items)
+    # Filtra aquellos items cuyo indice es par
+    por_pares_saltando_uno = filter(
+        lambda tupla_index_item: tupla_index_item[0] % 2 == 0,
+        [(idx, item) for idx, item in enumerate(por_pares)]
+    )
+    resultado = map(
+        lambda tupla_index_item: tupla_index_item[1],
+        por_pares_saltando_uno
+    )
+    return list(resultado)
