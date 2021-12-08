436a2799352518435b1473a094a157c54fafd38f
NanoSpicer <miquelrc95@outlook.es>
2021-12-08 20:32:54 +0100

    Adds implementation to perform crossover population

--- code/population.py
+++ code/population.py
@@ -7,9 +7,11 @@ Class Population():
     - mutation operator
     - crossover operator (reproduction)
 """
-
+import copy
+import random
 import numpy as np
 
+from utils import items_as_pairs
 import genotype as gen
 
 
@@ -36,20 +38,62 @@ class Population:
         self.results = None
         self.error = None
 
-    def select(self, n_sets: int, p_instances: int):
+    def select_parents(self):
         """
         Selection operator: selects n_sets sets of p_instances each one
         """
-        pass
+        population = list.copy(self.population)
+        shuffled_population = random.shuffle(population)
+        return items_as_pairs(shuffled_population)
 
-    def crossover(self):
+    def select_survivors(self):
+        return []  # todo
+
+    def crossover(self, list_of_parents):
+        # list_of_parents: [(p1, p2), (p3, p4)]
         """
         Crossover operator: creates a new set of genotype instances
         using the parents information
 
         :return:
         """
-        pass
+        for (parent1, parent2) in list_of_parents:
+            # parent1: genotype
+            num = random.randrange(0, 7*24)
+            child1 = self.make_child(parent1, parent2, num)
+            child2 = self.make_child(parent2, parent1, num)
+            self.population.append(child1)
+            self.population.append(child2)
+
+        self.mutate_population()
+
+    def make_child(self, parent1: gen.Genotype, parent2: gen.Genotype, num: int) -> gen.Genotype:
+        child1 = copy.deepcopy(parent1)
+        data_set2 = parent2.data_set
+
+        for (index_row, row) in enumerate(child1.data_set):
+            numeros_de_la_derecha = row[num:]
+            numeros_interesantes = list(
+                filter(
+                    lambda el: el != gen.AVAILABLE and el != gen.NOT_AVAILABLE,
+                    list(numeros_de_la_derecha)
+                )
+            )
+
+            row_p2 = data_set2[index_row]
+
+            for (index_col, _) in enumerate(numeros_de_la_derecha):
+                for cell in row_p2:
+                    if cell in numeros_interesantes:
+                        numeros_interesantes.remove(cell)
+                        row[num + index_col] = cell
+                        break
+        return child1
+
+    def mutate_population(self):
+        for genotype in self.population:
+            genotype.mutate()
+
 
     def fit(self, iterations: int = 10000):
         """
@@ -58,5 +102,12 @@ class Population:
         :param iterations: number of iterations to execute
         :return: nothing
         """
+        cpt = 0
+        while cpt < iterations:
+            list_of_parents = self.select_parents()
+            self.crossover(list_of_parents)
+            self.mutate_population()
+            self.select_survivors()
+            cpt += 1
 
-        pass
+        return 0.5
